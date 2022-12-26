f4f72e9b71554f62c6c487b7037eb4db9e06c3e6
OverHash <46231745+OverHash@users.noreply.github.com>
2022-12-26 22:42:55 +1300

    Add message reaction handling
    Handles message reaction events, keeping track of a new
    `REACTION_REQUIREMENT` config variable to initiate the starboard message
    when a single message gets enough reactions.

---
+++ migrations/20221225212932_starboard.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS starboard
+(
+	message_id		INTEGER			PRIMARY KEY NOT NULL,
+    starboard_id	INTEGER			NOT NULL
+);
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 use error_stack::{IntoReport, Result, ResultExt};
 use futures::stream::StreamExt;
-use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+use sqlx::{
+    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+    SqlitePool,
+};
 use std::{str::FromStr, sync::Arc};
 use twilight_cache_inmemory::{InMemoryCache, ResourceType};
 use twilight_gateway::{Event, Intents, Shard};
-use twilight_http::Client;
+use twilight_http::{request::channel::reaction::RequestReactionType, Client};
+use twilight_model::id::{marker::MessageMarker, Id};
 
 mod config;
 mod error;
 
 use config::ApplicationConfig;
 use error::{
-    ApplicationError, ConfigError, DatabaseError, DiscordError, EventError, MessageCreateError,
+    ApplicationError, ConfigError, DatabaseError, DiscordError, EventError, ReactionError,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), ApplicationError> {
+    // load `.env` file (if it exists) and subsequent config file into memory
     dotenvy::dotenv().ok();
 
-    let config = ApplicationConfig::load().change_context(ApplicationError::LoadConfig)?;
+    let config = Arc::new(ApplicationConfig::load().change_context(ApplicationError::LoadConfig)?);
 
+    // connect to sqlite database
     let connection_options = SqliteConnectOptions::from_str(&config.database_url)
         .into_report()
         .change_context(ApplicationError::LoadConfig)
@@ -37,8 +43,11 @@ async fn main() -> Result<(), ApplicationError> {
         pool.num_idle()
     );
 
-    // Let Discord know the intentions we need to run the bot
-    let intents = Intents::GUILDS | Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT;
+    // let Discord know the intentions we need to run the bot with the correct intentions
+    let intents = Intents::GUILDS
+        | Intents::GUILD_MESSAGES
+        | Intents::MESSAGE_CONTENT
+        | Intents::GUILD_MESSAGE_REACTIONS;
     let (cluster, mut events) = Shard::new(config.discord_token.clone(), intents);
     cluster
         .start()
@@ -46,44 +55,139 @@ async fn main() -> Result<(), ApplicationError> {
         .into_report()
         .change_context(ApplicationError::Discord(DiscordError::ConnectError))?;
 
-    let client = Arc::new(Client::new(config.discord_token));
-
-    // Since we only care about messages, make the cache only process messages.
-    let cache = InMemoryCache::builder()
-        .resource_types(ResourceType::MESSAGE)
-        .build();
+    // Since we only care about message emojis, make the cache only process messages.
+    let cache = Arc::new(
+        InMemoryCache::builder()
+            .resource_types(ResourceType::MESSAGE | ResourceType::REACTION)
+            .build(),
+    );
 
     // Startup an event loop to process each event in the event stream as they
     // come in.
+    let client = Arc::new(Client::new(config.discord_token.to_owned()));
     while let Some(event) = events.next().await {
+        let cache = cache.clone();
         // Update the cache.
         cache.update(&event);
 
         // Spawn a new task to handle the event
-        tokio::spawn(handle_event(event, client.clone()))
-            .await
-            .into_report()
-            .change_context(ApplicationError::Thread)?
-            .change_context(ApplicationError::Event)?;
+        tokio::spawn(handle_event(
+            event,
+            client.clone(),
+            cache,
+            pool.clone(),
+            config.clone(),
+        ))
+        .await
+        .into_report()
+        .change_context(ApplicationError::Thread)?
+        .change_context(ApplicationError::Event)?;
     }
 
     Ok(())
 }
 
-async fn handle_event(event: Event, http: Arc<Client>) -> Result<(), EventError> {
+async fn handle_event(
+    event: Event,
+    http: Arc<Client>,
+    cache: Arc<InMemoryCache>,
+    pool: SqlitePool,
+    config: Arc<ApplicationConfig>,
+) -> Result<(), EventError> {
     match event {
-        Event::MessageCreate(msg) if msg.content == "!ping" => {
-            http.create_message(msg.channel_id)
-                .content("Pong!")
-                .into_report()
-                .change_context(EventError::MessageCreateError(
-                    MessageCreateError::ContentResponseError,
-                ))?
+        Event::ReactionAdd(added) => {
+            // first check if message has already been starboard'd
+            let mut pool = pool
+                .acquire()
                 .await
                 .into_report()
-                .change_context(EventError::MessageCreateError(
-                    MessageCreateError::ReplyMessageError,
-                ))?;
+                .change_context(EventError::ReactionError(ReactionError::DatabaseConnect))?;
+
+            let message_id = added.message_id.to_string();
+
+            let starboard_id: Option<Id<MessageMarker>> = sqlx::query!(
+                r#"
+SELECT starboard_id
+FROM starboard
+WHERE message_id = ?
+				"#,
+                message_id
+            )
+            .fetch_optional(&mut pool)
+            .await
+            .into_report()
+            .change_context(EventError::ReactionError(
+                ReactionError::PreviousReactionCount,
+            ))?
+            .map(|id| -> std::result::Result<u64, _> { id.starboard_id.try_into() })
+            .transpose()
+            .into_report()
+            .change_context(EventError::ReactionError(
+                ReactionError::PreviousReactionCount,
+            ))?
+            .map(Id::new);
+
+            // if the starboard message was already created
+            // we do not need to do any more work
+            if starboard_id.is_some() {
+                return Ok(());
+            }
+
+            // retrieve the amount of reactions the message has now
+            // if the message was not in cache, we need to perform an http request to get it
+            let reactions = match cache.message(added.message_id) {
+                Some(m) => m.reactions().to_owned(),
+                None => {
+                    let message = http
+                        .message(added.channel_id, added.message_id)
+                        .await
+                        .into_report()
+                        .change_context(EventError::ReactionError(ReactionError::RetrieveMessage))?
+                        .model()
+                        .await
+                        .into_report()
+                        .change_context(EventError::ReactionError(
+                            ReactionError::RetrieveMessage,
+                        ))?;
+
+                    message.reactions
+                }
+            };
+
+            // check if we are above the config `reaction_requirement` threshold
+            // if not, early exit
+            let max_reactions = reactions.iter().map(|r| r.count).max().unwrap_or_default();
+            if max_reactions < config.reaction_requirement.into() {
+                println!(
+                    "message {message_id} has {max_reactions} max reactions for a single emoji now"
+                );
+
+                http.create_reaction(
+                    added.channel_id,
+                    added.message_id,
+                    &RequestReactionType::Unicode { name: "💀" },
+                )
+                .await
+                .expect("Failed to react");
+
+                return Ok(());
+            }
+
+            // add to starboard!
+            // todo
+            http.create_message(added.channel_id)
+                .content(&format!(
+                    "added reaction {:?}, which now has {reactions:?} unique emoji reactions",
+                    added.emoji
+                ))
+                .into_report()
+                .change_context(EventError::ReactionError(
+                    ReactionError::ContentResponseTooLong,
+                ))?
+                .reply(added.message_id)
+                .await
+                .into_report()
+                .change_context(EventError::ReactionError(ReactionError::ReplyMessage))?;
         }
         Event::ShardConnected(e) => {
             println!("Connected on shard {}", e.shard_id);
