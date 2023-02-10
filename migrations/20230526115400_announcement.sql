a35c53bbb3372f0ba2f4958ebe1190165f75384f
OverHash <46231745+OverHash@users.noreply.github.com>
2023-02-10 17:03:54 +1300

    Add announcement stream feature
    Allows users to add announcement streams to their `.env` to read and
    parse the ATOM feed and post a message in a channel whenever a new
    announcement was made.

---
+++ migrations/20230526115400_announcement.sql
@@ -0,0 +1,7 @@
+-- perform migration to add announcement feed
+-- we store the last_updated_time as unix epoch (in UTC milliseconds)
+CREATE TABLE IF NOT EXISTS announcement_feed
+(
+	id					TEXT		PRIMARY KEY NOT NULL,
+	last_updated_time	INTEGER		NOT NULL
+)
